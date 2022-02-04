399a5bb20f426fa7987ddfc6ba6be4f839dc34ab
JonasWilliam <jonaswilliamdelimasantos@gmail.com>
2022-02-04 17:12:54 -0300

    commit inicial

---
+++ xpto/src/test/java/br/com/mv/xpto/XptoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mv.xpto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
