955f9168580b9fca43703761be0144de073d10cb
Jonas <jonaswilliamdelimasantos@gmail.com>
2022-02-08 01:38:11 -0300

    adicionando as classes Conta e Movimentação. Incluindo Alterar de XPTO (classe generica).

--- xpto/src/main/java/br/com/mv/xpto/controller/XptoController.java
+++ xpto/src/main/java/br/com/mv/xpto/controller/XptoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,10 +20,10 @@ import br.com.mv.xpto.service.XptoService;
 @RestController
 @RequestMapping("/xpto")
 public class XptoController {
-	
+
 	@Autowired
 	XptoService xptoService;
-	
+
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
 	public List<Xpto> listarTodos() {
@@ -34,7 +35,7 @@ public class XptoController {
 	public Xpto salvar(@RequestBody Xpto xpto) {
 		return xptoService.salvar(xpto);
 	}
-	
+
 	@GetMapping(path = "/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public Xpto buscarPorId(@PathVariable int id) {
@@ -46,5 +47,14 @@ public class XptoController {
 	public void deletarPorId(@PathVariable int id) {
 		xptoService.excluir(id);
 	}
-	
+
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Xpto atualizar(@PathVariable int id, @RequestBody Xpto xpto) {
+		Xpto xptoAtual = xptoService.buscarPorId(id);
+		xptoAtual.setDescricao(xpto.getDescricao());
+		xptoAtual.setNome(xpto.getNome());
+		return xptoService.salvar(xptoAtual);
+	}
+
 }
